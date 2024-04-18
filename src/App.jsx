33407def4e795badb75896f79e7fb28e33bcbaaa
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-18 16:53:32 +1000

    App.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services.jsx";
+import Banner from "./components/Banner/Banner.jsx";
+import AppStore from "./components/AppStore/AppStore.jsx";
+import Testimonial from "./components/Testimonial/Testimonial.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Banner />
+      <AppStore />
+      <Testimonial />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
